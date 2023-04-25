03612e21229dd0f4546831c97d7e6fe1c3d0784a
Yash1Hingu <yash23hingu@gmail.com>
2023-04-26 00:33:28 +0530

    Navbar complete

---
+++ script.js
@@ -0,0 +1,7 @@
+const dropbox = document.getElementById("dropContainer");
+function dropdown(){
+    dropbox.classList.add("active");
+}
+function canceldown(){
+    dropbox.classList.remove("active");
+}
